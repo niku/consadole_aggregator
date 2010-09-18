513d3aa987c917dad38daa6917dbf8259913d856
niku <niku@niku.name>
2010-09-19 03:05:30 +0900

    投稿文字数が140文字を越えることがあるので，文字の切り詰めを定義

--- lib/consadole_aggregator.rb
+++ lib/consadole_aggregator.rb
@@ -5,11 +5,12 @@ require 'consadole_aggregator/entry.rb'
 require 'consadole_aggregator/live.rb'
 
 module ConsadoleAggregator
-  def self.truncate str, url
-    if str.size > 110
-      "#{str[0...107]}... #{url}"
+  def self.truncate str, url=''
+    if "#{str} #{url} #consadole".squeeze(' ').size > 140
+      truncated_size = 140 - "... #{url} #consadole".squeeze(' ').size
+      "#{str[0...truncated_size]}... #{url}".rstrip
     else
-      "#{str} #{url}"
+      "#{str} #{url}".rstrip
     end
   end
 end
