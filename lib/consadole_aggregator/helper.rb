24e043d318893aac845cf5a4527d26071d6d6f20
niku <niku@niku.name>
2011-05-01 14:45:20 +0900

    changed signature of Helper.concat

--- lib/consadole_aggregator/helper.rb
+++ lib/consadole_aggregator/helper.rb
@@ -1,10 +1,10 @@
 module ConsadoleAggregator
   class Helper
-    def self.concat text, url='', hashtag=''
-      base = "#{text} #{url} #{hashtag}".squeeze(' ').rstrip
+    def self.concat text, opt=Hash.new('')
+      base = "#{text} #{opt[:url]} #{opt[:hashtag]}".squeeze(' ').rstrip
       if base.size > 140
         over_size = base.size - 140
-        concat(omit(text, over_size), url, hashtag)
+        concat(omit(text, over_size), opt)
       else
         base
       end
--- spec/consadole_aggregator/helper_spec.rb
+++ spec/consadole_aggregator/helper_spec.rb
@@ -9,17 +9,17 @@ describe ConsadoleAggregator::Helper do
       it { should == 'い' * 140 }
     end
     context 'with argument 140 chars and 18 chars url' do
-      subject { ConsadoleAggregator::Helper.concat('ろ' * 140, 'http://example.jp/') }
+      subject { ConsadoleAggregator::Helper.concat('ろ' * 140, url:'http://example.jp/') }
       it { should have(140).item }
       it { should be_end_with 'ろ... http://example.jp/' }
     end
     context 'with argument 140 chars and 10 chars hashtag' do
-      subject { ConsadoleAggregator::Helper.concat('は' * 140, '', '#consadole') }
+      subject { ConsadoleAggregator::Helper.concat('は' * 140, hashtag:'#consadole') }
       it { should have(140).item }
       it { should be_end_with 'は... #consadole' }
     end
     context 'with argument 140 chars and 18 chars url and 10 chars hashtag' do
-      subject { ConsadoleAggregator::Helper.concat('に' * 140, 'http://example.jp/', '#consadole') }
+      subject { ConsadoleAggregator::Helper.concat('に' * 140, url:'http://example.jp/', hashtag:'#consadole') }
       it { should have(140).item }
       it { should be_end_with 'に... http://example.jp/ #consadole' }
     end
